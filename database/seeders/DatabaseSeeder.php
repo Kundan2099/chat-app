4619bf5aac5a3ceac65ed3a1c8de872f57cbf3ae
Kundan2099 <121600019+Kundan2099@users.noreply.github.com>
2024-08-20 17:38:41 +0530

    livewire

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,15 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+        ]);
 
-        $this->call([
-            AdminSeeder::class,
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test',
+            'email' => 'test@example.com',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,15 @@
 <?php
 
-use App\Http\Controllers\Web\Auth\AuthController;
-use App\Http\Controllers\Web\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', 'welcome');
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('view.welcome');
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-// Route::get('/hello', function () {
-//     return "kundan";
-// });
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
-Route::middleware(['guest'])->group(function () {
-
-    Route::get('login', [AuthController::class, 'viewLogin'])->name('web.view.login');
-    Route::post('login', [AuthController::class, 'handleLogin'])->name('web.handle.login');
-
-    Route::get('register', [AuthController::class, 'viewRegister'])->name('web.view.register');
-    Route::post('register', [AuthController::class, 'handleRegister'])->name('web.handle.register');
-
-    Route::get('forgot-password', [AuthController::class, 'viewForgotPassword'])->name('web.view.forgot.password');
-    Route::post('forgot-password', [AuthController::class, 'handleForgotPassword'])->name('web.handle.forgot.password');
-
-});
-
-Route::middleware(['auth'])->group(function() {
-
-Route::post('/logout', [DashboardController::class, 'handleLogout'])->name('web.handle.logout');
-
-    Route::get('/dashboard', [DashboardController::class, 'viewDashboard'])->name('web.view.dashboard');
-
-});
+require __DIR__.'/auth.php';
