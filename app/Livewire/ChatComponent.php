1c448528fd20670c08dee5efafe6badd5f175f19
Kundan2099 <121600019+Kundan2099@users.noreply.github.com>
2024-08-21 23:44:46 +0530

    message send

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Message;
 use App\Models\User;
 use Livewire\Component;
 use Request;
@@ -9,6 +10,9 @@ use Request;
 class ChatComponent extends Component
 {
     public $user;
+    public $sender_id;
+    public $receiver_id;
+    public $messages = '';
     public function render()
     {
         return view('livewire.chat-component');
@@ -16,6 +20,48 @@ class ChatComponent extends Component
 
     public function mount($id)
     {
+        $this->sender_id = auth()->user()->id;
+        $this->receiver_id = $id;
+
+        $messages = Message::where(function ($query) {
+            $query->where('sender_id', $this->sender_id)
+                ->where('receiver_id', $this->receiver_id);
+        })
+            ->orWhere(function ($query) {
+                $query->where('sender_id', $this->receiver_id)
+                    ->where('receiver_id', $this->sender_id);
+            })
+            ->with(['sender:id,name', 'receiver:id,name'])
+            ->get();
+
+        foreach ($messages as $message) {
+            $this->appendChatMessage($message);
+        }
+
+        // dd($this->messages);
+
         $this->user = User::where('id', $id)->first();
+    }
+
+    public function sendMessage()
+    {
+        $chatMessage = new Message();
+        $chatMessage->sender_id = $this->sender_id;
+        $chatMessage->receiver_id = $this->receiver_id;
+        $chatMessage->message = $this->message;
+        $chatMessage->save();
+
+        $this->message = '';
+    }
+
+
+    public function appendChatMessage($message)
+    {
+        $this->messages = [
+            'id' => $message->id,
+            'message' => $message->message,
+            'sender' => $message->sender->name,
+            'receiver' => $message->receiver->name,
+        ];
     }
 }
